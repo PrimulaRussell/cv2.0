75eb05226df0c3b277dd562fc4f1c8e7aaa706e0
Primula <primularussell@gmail.com>
2021-03-31 19:22:33 -0700

    created base layout

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Opener from './Opener'
+import '../styles/container.css'
+
+const Container = () => {
+    return (
+        <div className="container">
+            <Opener></Opener>
+        </div>
+    )
+}
+
+export default Container
---
+++ src/components/Opener.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/opener.css';
+
+const Opener = () => {
+    return (
+        <div className="background">
+            <h1>Hello, my name is Tara. I'm a full-stack web developer.</h1>
+            
+            
+        </div>
+    )
+}
+
+export default Opener
+
